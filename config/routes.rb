5ff34fcaaee4b65a44fb9aa50a048afe79babf67
ImagineErin <thinkerin1@gmail.com>
2019-03-29 10:56:46 -0400

    final project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :donations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#index'
+  root to: 'donations#index'
   get '/groups', to: 'groups#index'
   get '/schedule', to: 'schedule#index'
 end
